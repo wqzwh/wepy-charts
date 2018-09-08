cee0616a315180bde2b94420984e39791dd9dd81
wqzwh <1424195010@qq.com>
2018-09-08 19:04:35 +0800

    删除无用文件

--- src/public/conf.js
+++
@@ -1,8 +0,0 @@
-const HOST = process.env.NODE_ENV === 'development' ? 'http://sop-gateway.jd.com/focus/staging/mock' : ''
-const PRO_HOST = ""
-const MOCK_HOST = 'http://sop.jd.com/'
-export {
-  HOST,
-  PRO_HOST,
-  MOCK_HOST
-}
